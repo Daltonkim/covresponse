4717e638fbc72d966efd4612dd07a81c5e83f83b
Daltonkim <dalton.korir@incentro.com>
2020-04-23 14:30:02 +0300

    header set up

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Collapse,
   Navbar,
@@ -10,47 +10,50 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
-} from 'reactstrap';
+  DropdownItem,
+} from "reactstrap";
+import SideNavBar from "../side-bar";
+import './index.scss'
 
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
   return (
-    <div>
-      <Navbar color="dark" dark expand="md">
-        <NavbarBrand href="/">Code the Curve Website - Team 13 | Mbali Health</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar >
-          <Nav className="ml-auto" navbar >
-            <NavItem>
-              <NavLink href="/">Settings</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/">Notifications</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                John Smith
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
+    <div className="navbar__wrapper">
+      <div className="navbar__side-wrapper">
+        <SideNavBar />
+      </div>
+      <div className="navbar__main-menu">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">
+            Code the Curve Website - Team 13 | Mbali Health
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Settings</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Notifications</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  John Smith
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     </div>
   );
-}
+};
 export default Header;
